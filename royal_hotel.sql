4d2d0e4663796ff4c2eb1488918272340bed441e
kashika2314 <139805936+kashika2314@users.noreply.github.com>
2023-07-18 00:36:59 +0530

    Add files via upload

---
+++ royal_hotel.sql
@@ -0,0 +1,207 @@
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `mobile` int(11) NOT NULL
+) 
+--
+-- Inserting values`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `mobile`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin@123', 77874512);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `double_ac`
+--
+
+CREATE TABLE `double_ac` (
+  `s_no` int(11) NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `holder_name` varchar(100) DEFAULT NULL,
+  `holder_id` int(11) DEFAULT NULL,
+  `holder_mobile` varchar(100) DEFAULT NULL,
+  `holder_address` varchar(250) DEFAULT NULL,
+  `child` int(11) DEFAULT NULL,
+  `adult` int(11) DEFAULT NULL,
+  `in_date` varchar(100) DEFAULT NULL,
+  `out_date` varchar(100) DEFAULT NULL,
+  `status` int(11) NOT NULL
+) 
+
+--
+Inserting values`
+--
+
+INSERT INTO `double_ac` (`s_no`, `room_no`, `holder_name`, `holder_id`, `holder_mobile`, `holder_address`, `child`, `adult`, `in_date`, `out_date`, `status`) VALUES
+(1, 251, 'b', 545, '5454', 'gafg', 2, 1, '25-06-2020', '29-06-2020', 1),
+(2, 252, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(4, 253, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(5, 254, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `double_non_ac`
+--
+
+CREATE TABLE `double_non_ac` (
+  `s_no` int(11) NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `holder_name` varchar(100) DEFAULT NULL,
+  `holder_id` int(11) DEFAULT NULL,
+  `holder_mobile` varchar(100) DEFAULT NULL,
+  `holder_address` varchar(250) DEFAULT NULL,
+  `child` int(11) DEFAULT NULL,
+  `adult` int(11) DEFAULT NULL,
+  `in_date` varchar(100) DEFAULT NULL,
+  `out_date` varchar(100) DEFAULT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+Inserting values`
+
+--
+
+INSERT INTO `double_non_ac` (`s_no`, `room_no`, `holder_name`, `holder_id`, `holder_mobile`, `holder_address`, `child`, `adult`, `in_date`, `out_date`, `status`) VALUES
+(1, 201, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(2, 202, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(4, 203, 'Akshay ', 74125, '85245698', 'Address of akshay', 0, 2, '25-05-2020', '30-05-2020', 1),
+(5, 204, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `single_ac`
+--
+
+CREATE TABLE `single_ac` (
+  `s_no` int(11) NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `holder_name` varchar(100) DEFAULT NULL,
+  `holder_id` int(11) DEFAULT NULL,
+  `holder_mobile` varchar(100) DEFAULT NULL,
+  `holder_address` varchar(250) DEFAULT NULL,
+  `child` int(11) DEFAULT NULL,
+  `adult` int(11) DEFAULT NULL,
+  `in_date` varchar(100) DEFAULT NULL,
+  `out_date` varchar(100) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Inserting values`
+
+--
+
+INSERT INTO `single_ac` (`s_no`, `room_no`, `holder_name`, `holder_id`, `holder_mobile`, `holder_address`, `child`, `adult`, `in_date`, `out_date`, `status`) VALUES
+(1, 151, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(2, 152, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(4, 153, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(5, 154, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `single_non_ac`
+--
+
+CREATE TABLE `single_non_ac` (
+  `s_no` int(11) NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `holder_name` varchar(100) NOT NULL,
+  `holder_id` int(11) NOT NULL,
+  `holder_mobile` varchar(100) NOT NULL,
+  `holder_address` varchar(250) NOT NULL,
+  `child` int(11) NOT NULL,
+  `adult` int(11) NOT NULL,
+  `in_date` varchar(100) NOT NULL,
+  `out_date` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL
+) 
+
+--
+`
+--
+
+INSERT INTO `single_non_ac` (`s_no`, `room_no`, `holder_name`, `holder_id`, `holder_mobile`, `holder_address`, `child`, `adult`, `in_date`, `out_date`, `status`) VALUES
+(1, 101, '', 0, '', '', 0, 0, '', '', 0),
+(2, 102, 'Pramod Sharma', 1010, '9878451245', 'xyz nagar , rajasthan', 2, 2, '29-04-2020', '31-04-2020', 1),
+(3, 103, '', 0, '', '', 0, 0, '', '', 0),
+(4, 104, '', 0, '', '', 0, 0, '', '', 0);
+
+--
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+--
+ALTER TABLE `double_ac`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+
+--
+ALTER TABLE `double_non_ac`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+
+--
+ALTER TABLE `single_ac`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+
+--
+ALTER TABLE `single_non_ac`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `double_ac`
+--
+ALTER TABLE `double_ac`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `double_non_ac`
+--
+ALTER TABLE `double_non_ac`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `single_ac`
+--
+ALTER TABLE `single_ac`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `single_non_ac`
+--
+ALTER TABLE `single_non_ac`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
